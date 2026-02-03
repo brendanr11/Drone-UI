c474eaaf54d922ba262c032780523eb8857332b6
brendanreilly <brendantreilly11@gmail.com>
2026-02-03 13:59:22 -0500

    Utils fix for render deploy.

--- app/src/lib/utils.ts
+++ app/src/lib/utils.ts
@@ -1,6 +1,8 @@
-import { clsx, type ClassValue } from "clsx";
+import type { ClassValue } from "clsx";
+import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+// Tailwind-friendly className combiner used by shadcn/ui components
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
